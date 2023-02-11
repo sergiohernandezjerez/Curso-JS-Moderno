c414508fd5b0d7185ea74db0c9894a2c3069e6c4
Sergio Hernández <sergiohernandezjerez@gmail.com>
2023-02-11 18:32:26 +0100

    añado div al html para la simulación de envio de email, hoja de estilos

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -12,21 +12,32 @@ document.addEventListener('DOMContentLoaded', function(){
     const inputMensaje = document.querySelector('#mensaje');
     const formulario = document.querySelector('#formulario');
     const btnSubmit = document.querySelector('#formulario button[type="submit"]');
+    const btnReset = document.querySelector('#formulario button[type="reset"]');
 
     //asignar eventos
-    inputEmail.addEventListener('blur', validar);
-    inputMensaje.addEventListener('blur', validar);
-    inputAsunto.addEventListener('blur', validar);
+    inputEmail.addEventListener('input', validar);
+    inputMensaje.addEventListener('input', validar);
+    inputAsunto.addEventListener('input', validar);
+    btnReset.addEventListener('click', function(e){
+        e.preventDefault();
+        email[e.target.name] = '';
+        
+        formulario.reset();
+        comprobarEmail();
+
+    })
 
     function validar(e){
         if(e.target.value.trim() === ''){
             mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
+            email[e.target.name] = '';
             comprobarEmail();
             return;
         }
         
         if(e.target.id === 'email' && !validarEmail(e.target.value)){
             mostrarAlerta('El email no és válido', e.target.parentElement);
+            email[e.target.name] = '';
             comprobarEmail();
             return;
         }
@@ -35,7 +46,7 @@ document.addEventListener('DOMContentLoaded', function(){
         limpiarAlerta(e.target.parentElement);
 
         //Asignar los valores
-        email[e.tarnget.name] = e.target.value.trim().toLowerCase();
+        email[e.target.name] = e.target.value.trim().toLowerCase();
         
         //comprobar el objeto email
         comprobarEmail();
@@ -71,10 +82,11 @@ document.addEventListener('DOMContentLoaded', function(){
         if(Object.values(email).includes('')){
             btnSubmit.classList.add('opacity-50');
             btnSubmit.disable = true;
-        }else{
-            btnSubmit.classList.remove('opacity-50');
-            btnSubmit.disable = false;
+            return;
         }
+        btnSubmit.classList.remove('opacity-50');
+        btnSubmit.disable = false;
+        
 
     }
 });
